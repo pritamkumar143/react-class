2886b2cf4992b53466cee4fdf358ffecf39391f5
Pritam Kumar <pritamkunar13@gmail.com>
2024-10-15 17:16:09 +0530

    use isActive and style in navlink

--- routing/src/components/Nav.jsx
+++ routing/src/components/Nav.jsx
@@ -1,13 +1,25 @@
 // Nav.jsx
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Nav = () => {
     return (
-       <div className='bg-pink-400 flex justify-evenly items-center text-2xl h-16'>
-            <Link to="/" className="text-white hover:text-gray-400">Home</Link>
-            <Link to="/about" className="text-white hover:text-gray-400">About</Link>
-            <Link to="/service" className="text-white hover:text-gray-400">Service</Link>
+        <div className='bg-pink-400 flex justify-evenly items-center text-2xl h-16'>
+            <NavLink
+                style={(e) => e.isActive? {textDecoration:'underline'}:{}}
+                className={(e) => e.isActive ?  'text-blue-800' : ''}
+                to='/'
+            >
+                Home
+            </NavLink>
+            <NavLink
+                style={(e) => e.isActive ? { textDecoration: 'underline' } : {}}
+                className={(e) => e.isActive ? 'text-green-600' : ''}
+                to="/about">About</NavLink>
+            <NavLink
+                style={(e) => e.isActive ? { textDecoration: 'underline' } : {}}
+                className={(e) => e.isActive ? 'text-yellow-500' : ''}
+                to="/service" >Service</NavLink>
         </div>
     );
 };
