f9da6f9542fd3596c13abda74a275cded06f6d66
Pritam Kumar <pritamkunar13@gmail.com>
2024-10-15 17:01:54 +0530

    hadle form submit and navigate

--- routing/src/components/Service.jsx
+++ routing/src/components/Service.jsx
@@ -1,9 +1,23 @@
 // Service.jsx
+import {useNavigate } from 'react-router-dom'
 const Service = () => {
+    const navigate = useNavigate()
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        console.log('submiteddd');
+        
+
+        navigate('/')
+        // navigate(1)
+
+    }
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-yellow-100">
             <h1 className="text-4xl font-bold text-yellow-600">Our Services</h1>
             <p className="mt-4 text-lg">Check out the services we offer.</p>
+            <form onSubmit={SubmitHandler}>
+                <button className="rounded-full bg-pink-500 p-4">Submit</button>
+            </form>
         </div>
     );
 };
