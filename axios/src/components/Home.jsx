ef8eae1c03022be978876bdc10e1911f1af135ad
Pritam Kumar <pritamkunar13@gmail.com>
2024-10-19 22:00:22 +0530

    set up api fetch from fakestoreapi

--- axios/src/components/Home.jsx
+++ axios/src/components/Home.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 
 
 const Home = () => {
+  useEffect(() => {
+    console.log('App.jsx mounted');
+
+    return () => {
+      console.log('App.jsx is unmounted');
+
+    }
+
+  }, [])
   return (
     <div>
       
--- axios/src/components/Item.jsx
+++ axios/src/components/Item.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import axios  from "../utils/axios";
 
 
 const Item = () => {
+
+  const [products, setproducts] = useState(null)
+
+  const getproducts = async () => {
+    try {
+      const { data } = await axios.get('/products')
+      console.log(data);
+      setproducts(data);
+
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
   return (
     <div>
-      
+      <h1>pro</h1>
     </div>
   )
 }
